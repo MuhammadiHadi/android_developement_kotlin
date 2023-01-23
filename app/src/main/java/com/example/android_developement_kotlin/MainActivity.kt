7900c6dea5ab1ecd893137ccf9865a4de61c6770
HYPER <meerhadikhan364@gmail.com>
2023-01-23 12:56:19 +0500

    counter exmaple

--- app/src/main/java/com/example/android_developement_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/android_developement_kotlin/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.android_developement_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var x=0;
+       val button=findViewById<Button>(R.id.button);
+       val count=findViewById<Button>(R.id.button2);
+        var text=findViewById<TextView>(R.id.count);
+
+
+
+       count.setOnClickListener(){
+           text.setText("${x++}");
+       }
+       button.setOnClickListener(){
+
+
+           intent =Intent(this,MainActivity2::class.java);
+           startActivity(intent);
+       }
+
     }
+
+
+
+
+
+
 }
+
+
+
